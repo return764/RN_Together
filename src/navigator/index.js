fbe9985a7c242775c98655db6624237ec7ac8fdb
return764 <543851436@qq.com>
2022-08-03 17:58:57 +0800

    refactor: extract tabScreens array

--- src/navigator/index.js
+++ src/navigator/index.js
@@ -8,46 +8,61 @@ import RockIcon from '../assets/icon/rock.svg';
 import ShopScreen from '../pages/ShopScreen';
 import ToosIcon from '../assets/icon/toos.svg';
 import SettingScreen from '../pages/SettingScreen';
+import {Text} from 'react-native';
 
 const Tab = createBottomTabNavigator();
-const iconSize = 30;
+const iconSize = 25;
+
+const tabScreens = [
+  {
+    name: 'Home',
+    component: HomeScreen,
+    options: {
+      title: '主页',
+      tabBarLabel: () => <Text>主页</Text>,
+      tabBarIcon: () => <DashboardIcon height={iconSize} width={iconSize} />,
+    },
+  },
+  {
+    name: 'Task',
+    component: TaskScreen,
+    options: {
+      title: '任务',
+      tabBarLabel: () => <Text>任务</Text>,
+      tabBarIcon: () => <TaskIcon height={iconSize} width={iconSize} />,
+    },
+  },
+  {
+    name: 'Shop',
+    component: ShopScreen,
+    options: {
+      title: '商店',
+      tabBarLabel: () => <Text>商店</Text>,
+      tabBarIcon: () => <RockIcon height={iconSize} width={iconSize} />,
+    },
+  },
+  {
+    name: 'Setting',
+    component: SettingScreen,
+    options: {
+      title: '设置',
+      tabBarLabel: () => <Text>设置</Text>,
+      tabBarIcon: () => <ToosIcon height={iconSize} width={iconSize} />,
+    },
+  },
+];
 
 const MyNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Group screenOptions={{presentation: 'transparentModal'}}>
+      {tabScreens.map(({name, component, options}) => (
         <Tab.Screen
-          name="Home"
-          options={{
-            tabBarIcon: () => (
-              <DashboardIcon height={iconSize} width={iconSize} />
-            ),
-          }}
-          component={HomeScreen}
+          navigationKey={name}
+          name={name}
+          component={component}
+          options={options}
         />
-        <Tab.Screen
-          name="Task"
-          options={{
-            tabBarIcon: () => <TaskIcon height={iconSize} width={iconSize} />,
-          }}
-          component={TaskScreen}
-        />
-      </Tab.Group>
-
-      <Tab.Screen
-        name="Shop"
-        options={{
-          tabBarIcon: () => <RockIcon height={iconSize} width={iconSize} />,
-        }}
-        component={ShopScreen}
-      />
-      <Tab.Screen
-        name="Setting"
-        options={{
-          tabBarIcon: () => <ToosIcon height={iconSize} width={iconSize} />,
-        }}
-        component={SettingScreen}
-      />
+      ))}
     </Tab.Navigator>
   );
 };
